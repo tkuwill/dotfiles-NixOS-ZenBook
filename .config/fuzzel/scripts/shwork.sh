fd8369f5d519dd8c242d91a4ab35fd6e31fe7fb1
tkuwill <billwang1023@gmail.com>
2024-06-02 16:32:52 +0800

    Add the config to show workspaces has been opened in dunst.

--- .config/fuzzel/scripts/shwork.sh
+++ .config/fuzzel/scripts/shwork.sh
@@ -2,9 +2,10 @@
 # Description: show workspaces in use ny dunst.
 # <a href="https://www.flaticon.com/free-icons/workspace" title="workspace icons">Workspace icons created by Uniconlabs - Flaticon</a>
 curwork() {
-  hyprctl workspaces | grep 'workspace' | awk '{print $3}' | sort -n | xargs
+  # hyprctl workspaces | grep 'workspace' | awk '{print $3}' | sort -n | xargs
+  niri msg workspaces | sed '1 d' | xargs
 }
 
 curwork
 
-dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/network.png -t 8000 "Space $(curwork) in use."
+dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/network.png -t 8000 "Spaces $(curwork) Now in use."
