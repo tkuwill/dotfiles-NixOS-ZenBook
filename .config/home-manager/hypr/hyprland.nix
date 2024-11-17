907be884fefa5bc6ce9820aea9967bd5d3bab421
tkuwill <billwang1023@gmail.com>
2024-11-17 08:25:18 +0800

    Update flake.lock. Fix hypr's config.

--- .config/home-manager/hypr/hyprland.nix
+++ .config/home-manager/hypr/hyprland.nix
@@ -44,10 +44,6 @@
       }
       decoration {
         rounding = 0
-        drop_shadow = yes
-        shadow_range = 4
-        shadow_render_power = 3
-        col.shadow = rgba(1a1a1aee)
       }
       animations {
         enabled = no
--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -198,7 +198,6 @@
     dmidecode
     qemu
     quickemu
-    #
     # niri
     vim-full # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     ## icon theme
